e6f7b004380a353065cfd8b72b8b1c45e1958438
unknown <phansenwps@gmail.com>
2016-11-03 23:49:38 -0400

    mips core adequate for R type. Need to implement Control

--- lab4-start/mips_core.v
+++ lab4-start/mips_core.v
@@ -22,7 +22,8 @@ module mips_core(clk, rst, mem_rd_data, mem_addr, mem_wr_data, instr, PCWriteCon
 	//buses
 	wire PC_ena, ALU_zero;
 	wire [31:0] PC_in, PC_out, MDR_out, A_out, B_out, ALU_out, ALUout_out, reg_file_wr_data, reg_file_rd_data1, reg_file_rd_data2,
-					ALU_in1, ALU_in2, ALU_op;
+					ALU_in1, ALU_in2;
+	wire [3:0] ALUControl;
 	wire [4:0] reg_file_rd_addr1, reg_file_rd_addr2, reg_file_wr_addr;
 					
 	assign mem_wr_data = B_out;
@@ -33,9 +34,11 @@ module mips_core(clk, rst, mem_rd_data, mem_addr, mem_wr_data, instr, PCWriteCon
 	assign reg_file_wr_addr = RegDst ? instr[15:11] : instr[20:16];
 	assign reg_file_wr_data = MemtoReg ? MDR_out : ALUout_out;
 	assign ALU_in1 = ALUSrcA ? A_out : PC_out;
-	assign ALU_in2 = ALUSrcB[1] ? (ALUSrcB[0] ? /* TODO: shift left */ : /* TODO: sign extend */) : (ALUSrcB[0] ? 32'h4 : B_out);
-	assign PC_in = PCSource[1] ? (PCSource[0] ?  : ) : (PCSource[0] ?  : );
+	assign ALU_in2 = ALUSrcB[1] ? (ALUSrcB[0] ? 0/* TODO: shift left */ : 0/* TODO: sign extend */) : (ALUSrcB[0] ? 32'h4 : B_out);
+	assign PC_in = PCSource[1] ? (PCSource[0] ? 0 : 0/* TODO: jump addr */) : (PCSource[0] ? ALUout_out : ALU_out);
 	
+	//sign extend and shift lefts
+	/* TODO */
 	
 	//instantiating registers
 	register #(.N(32)) PC (.clk(clk), .d(/* TODO */), .q(PC_out), .rst(rst), .ena(PC_ena));
@@ -46,7 +49,10 @@ module mips_core(clk, rst, mem_rd_data, mem_addr, mem_wr_data, instr, PCWriteCon
 	register #(.N(32)) ALUout (.clk(clk), .d(ALU_out), .q(ALUout_out), .rst(rst), .ena(`ALWAYS_ENA));
 	
 	//instantiating ALU
-	alu #(.N(32)) ALU (.x(ALU_in1), .y(ALU_in2), .op_code(ALU_op), .z(ALU_out), .zero(ALU_zero));
+	alu #(.N(32)) ALU (.x(ALU_in1), .y(ALU_in2), .op_code(ALUControl), .z(ALU_out), .zero(ALU_zero));
+	
+	//instantiating ALU control
+	alu_control ALU_CONTROL (.ALUOp(ALUOp), .funct(instr[5:0]), .ALUControl(ALUControl));
 	
 	//instantiating Register File
 	register_file #(.N(32)) REGISTER_FILE (.clk(clk), .rst(rst), .rd_addr0, rd_addr1, wr_addr, rd_data0, rd_data1, wr_data, wr_ena);
--- lab4-start/mips_defines.v
+++ lab4-start/mips_defines.v
@@ -1,5 +1,15 @@
 `ifndef MIPS_DEFINES
 `define MIPS_DEFINES
-`define ALWAYS_ENA = 1
+`define ALWAYS_ENA 1
+`define ADD 6'd32
+`define SUB 6'd34
+`define AND 6'd36
+`define OR 6'd37
+`define SLT 6'd42
+`define XOR 6'd38
+`define NOR 6'd39
+`define SLL 6'd0
+`define SRL 6'd2
+`define SRA 6'd3
 
 `endif
